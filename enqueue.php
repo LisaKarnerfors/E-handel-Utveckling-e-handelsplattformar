67a49367e3d2b84ba691e6662729c8acdc31beb4
Lisa Karnerfors <91253216+LisaKarnerfors@users.noreply.github.com>
2022-05-24 21:01:58 +0200

    Add files via upload

---
+++ enqueue.php
@@ -0,0 +1,60 @@
+<?php 
+
+function register_scripts(){
+
+    //register styles with update time 
+    wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory().'/style.css'), 'all');
+    wp_register_style('sports-css', get_stylesheet_uri(), [], filemtime(get_template_directory().'/css/sports.css'), 'all');
+/*     wp_register_style('styles-css', get_stylesheet_uri(), [], filemtime(get_template_directory().'/css/styles.css'), 'all');
+ */    wp_register_style('bootstrap-css', get_template_directory_uri().'/asset/css/bootstrap.min.css', [], false, 'all');
+    wp_register_style('footer-css', get_template_directory_uri().'/css/footer.css', [], false, 'all');
+    wp_register_style('product-card-css', get_template_directory_uri().'/css/product-card.css', [], false, 'all');
+
+
+    //register scripts with update time
+    wp_register_script('bootstrap-js', get_template_directory_uri().'/js/jquery.js', ['jquery'],null, true);
+    wp_register_style('script-js', get_template_directory_uri().'/js/script.js', [], filemtime(get_template_directory().'/js/script.js'), true);
+   
+
+    //enqueue styles
+    wp_enqueue_style('style-css');
+    wp_enqueue_style('sports-css');
+
+    wp_enqueue_style('style-min-css');
+    wp_enqueue_style('styles-css');
+    wp_enqueue_style('bootstrap-css');
+    wp_enqueue_style('footer-css');
+    wp_enqueue_style('product-card-css');
+    
+    //enqueue scripts
+    wp_enqueue_script('script-js');
+    wp_enqueue_script('bootstrap-js');
+    
+ }
+
+ add_action( 'wp_enqueue_scripts', 'register_scripts');
+
+// Köar in CSS & JS filer
+/*  function load_scripts() {
+
+    // CSS
+    wp_enqueue_style("bootstrap", get_template_directory_uri()."/asset/css/bootstrap.min.css");
+    wp_enqueue_style("font-awesome", get_template_directory_uri()."/css/font-awesome.css");
+    wp_enqueue_style("style", get_template_directory_uri()."/style.css");
+    
+    // JS
+    wp_enqueue_script("bootstrap", get_template_directory_uri()."/asset/js/bootstrap.min.js");
+    wp_enqueue_script("bootstrap", get_template_directory_uri()."/asset/js/bootstrap.js");
+    wp_enqueue_script('jquery', get_template_directory_uri().'/js/jquery.js'); 
+    wp_enqueue_script('script', get_template_directory_uri().'/js/script.js'); 
+    wp_enqueue_style('bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css');
+    wp_enqueue_script( 'boot1','https://code.jquery.com/jquery-3.3.1.slim.min.js', array( 'jquery' ),'',true );
+    wp_enqueue_script( 'boot2','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array( 'jquery' ),'',true );
+    wp_enqueue_script( 'boot3','https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', array( 'jquery' ),'',true );
+
+}
+
+add_action("wp_enqueue_scripts", "load_scripts");
+ */
+
+?> 
---
+++ functions.php
@@ -0,0 +1,142 @@
+<?php 
+
+//Inkluderar filen som köar in alla script
+include('enqueue.php');
+
+
+// Adderar features, menu & widegt-support på sidor
+add_theme_support('post-thumbnails');
+add_theme_support('menus');
+add_theme_support('widgets');
+
+
+function add_woocommerce_support(){
+    add_theme_support('woocommerce');
+}
+
+add_action("after_setup_theme", "add_woocommerce_support");
+
+
+// Functions Menus 
+function register_menu() {
+        register_nav_menu("headermenu", "Header");
+        register_nav_menu("footermenu", "Footer");
+        register_nav_menu("sidemenu", "");
+    }
+
+add_action("after_setup_theme", "register_menu");
+
+
+
+// Functions Image
+/* add_image_size("blogImage_size", 635, 423, true );  */
+
+// Functions sidebar
+function widget_sidebar() {
+    
+    register_sidebar( 
+        [
+        'name' => 'Kontakt Uppgifter',
+        'id' => 'contact',
+        'description' => 'info blabla...',
+        'before_widget' => '<div>',
+        'after_widget' => '</div>'
+        ]
+      );
+    
+    register_sidebar( 
+        [
+        'name' => 'Copy right',
+        'id' => 'copy',
+        'description' => 'info blabla...',
+        'before_widget' => '<div>',
+        'after_widget' => '</div>'
+        ]
+    );
+    
+    register_sidebar( 
+        [
+        'name' => 'Social Media',
+        'id' => 'social',
+        'description' => 'info blabla...',
+        'before_widget' => '<div>',
+        'after_widget' => '</div>'
+        ]
+    );
+}
+
+add_action("after_setup_theme", "widget_sidebar",);
+
+// Functions widgets 
+function widget_footer() {
+
+    register_sidebar(
+        [
+            "name" => "wigdet footer1",
+            "id" => "widgetfooter1",
+            "description" => "About us"
+        ]
+    );
+}
+add_action("after_setup_theme", "widget_footer");
+
+
+/* Kassan */
+/* add_action('wp','remove_oceanwp_functions'); */
+
+
+/* function remove_oceanwp_functions(){
+
+    if (is_checkout()){
+
+        remove_all_actions('ocean_page_header');
+        remove_all_actions('ocean_footer');
+    
+        remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
+
+    }     */
+       
+
+/* add_filter('woocommerce_checkout_fields','hj_override_checkout_fields'); */
+/*         function hj_override_checkout_fields($fields){
+
+            unset(
+
+                $fields['order']['order_comments'],
+                $fields['billing']['billing_company'],
+                $fields['billing']['billing_country'],
+                $fields['billing']['billing_address_2'],
+
+                $fields['shipping']['shipping_company'],
+                $fields['shipping']['shipping_first_name'],
+                $fields['shipping']['shipping_last_name'],
+                $fields['shipping']['shipping_country'],
+
+                $fields['shipping']['shipping_address_1'],
+                $fields['shipping']['shipping_address_2'],
+                $fields['shipping']['shipping_postcode'],
+                $fields['shipping']['shipping_city'],
+
+                
+            );
+
+            return $fields;
+
+        }
+
+
+        add_action( 'woocommerce_review_order_after_payment', 'hj_edit_cart_checkout' ); */
+
+
+    /*     function hj_edit_cart_checkout(){
+
+            echo '<a href="'.hj_edit_cart_checkout().'"/a>';
+
+        }
+
+    } */
+
+
+
+?>
+
